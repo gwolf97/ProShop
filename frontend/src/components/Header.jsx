90c2f46d28c2682d5d1dccd4a5e62b0cc3bf39a8
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-06-18 17:53:59 -0500

    React router and links implemented on products and navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Nav, NavDropdown, Navbar, Form, FormControl, Button } from 'react-bootstrap'
+import {LinkContainer} from "react-router-bootstrap"
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
 
 const Header = () => {
@@ -7,12 +8,18 @@ const Header = () => {
     <header>
     <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+    <LinkContainer to="/">
+        <Navbar.Brand>ProShop</Navbar.Brand>
+    </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <NavbarCollapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-            <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
+        <LinkContainer to ="/cart">
+            <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/login">
+            <Nav.Link><i className="fas fa-user"></i>Sign In</Nav.Link>
+        </LinkContainer>
         </Nav>
         </NavbarCollapse>
         </Container>

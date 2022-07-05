95a015db4e339e61b3fc79cd7d9f94c0dcc2fea5
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-07-05 18:27:56 -0500

    Show orders on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -81,4 +81,13 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     }
     })
 
-export {addOrderItems, getOrderById, updateOrderToPaid}
+//@desc Get logged in user orders
+//@route GET /api/orders/myorders/:id
+//@access Private
+
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({user: req.params.id})
+    res.json(orders)
+    })
+
+export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 const router = express.Router()
-import { addOrderItems, getOrderById, updateOrderToPaid } from "../controllers/orderController.js"
+import { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid } from "../controllers/orderController.js"
 import {protect} from "../middlewear/authMiddleware.js"
 
 router.route("/").post(protect, addOrderItems)
 router.route("/:id").get(protect, getOrderById)
 router.route("/:id/pay").put(protect, updateOrderToPaid)
+router.route("/myorders/:id").get(protect, getMyOrders)
 
 export default router

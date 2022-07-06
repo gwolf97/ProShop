4eb67e93f7edc4d5c5824cd8ef9a025985276781
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-07-06 15:01:10 -0500

    Admin get user and update user endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from "express"
 const router = express.Router()
-import {authUser,registerUser, getUserProfile, updateUserProfile, getAllUsers, deleteUser} from "../controllers/userController.js"
+import {authUser,registerUser, getUserProfile, updateUserProfile, getAllUsers, deleteUser, getUserById, updateUser} from "../controllers/userController.js"
 import {protect, admin} from "../middlewear/authMiddleware.js"
 
 router.route("/").post(registerUser).get(protect, admin, getAllUsers)
 router.post("/login", authUser)
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route("/:id").delete(protect, admin, deleteUser)
-
+router.route("/:id").delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 export default router
